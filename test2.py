b24ab728393d9d5c1f09bb58b7f394865f84a91e
Spen <87278623+SpenGUI@users.noreply.github.com>
2022-10-02 17:14:53 +0100

    test 2 and words
    words is the api i use

---
+++ test2.py
@@ -0,0 +1,23 @@
+import random
+import webbrowser
+randomWord = "deez nuts"
+
+def load_words():
+    with open('words.txt') as word_file:
+        valid_words = set(word_file.read().split())
+
+def getNewWord():
+    global randomWord
+    if __name__ == '__main__':
+        english_words = load_words()
+        # demo print
+        randomWord = random.choice(open('words.txt').read().split()).strip()
+
+getNewWord()
+
+marge = ("https://www.dictionary.com/browse/",randomWord)
+url = "".join(marge)
+
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito'
+
+webbrowser.get(chrome_path).open_new(url)
